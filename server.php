045f6b86f73f0d78899508a26c981b0dc56a4164
Matteo Cruz <mattfreestyle121@gmail.com>
2023-04-27 21:26:03 +0200

    lien hrf

--- server.php
+++ server.php
@@ -1,24 +1,24 @@
 <?php
 session_start();
 
-// initializing variables
+// Initialisation
 $username = "";
 $email    = "";
 $errors = array(); 
 
-// connect to the database
+// Connexion à la database
 $db = mysqli_connect('localhost', 'root', '', 'project');
 
-// REGISTER USER
+// Page inscription
 if (isset($_POST['reg_user'])) {
-  // receive all input values from the form
+  
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $email = mysqli_real_escape_string($db, $_POST['email']);
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
+  // Test d'erreurs
+  
   if (empty($username)) { array_push($errors, "Identifiant nécessaire"); }
   if (empty($email)) { array_push($errors, "Email nécessaire"); }
   if (empty($password_1)) { array_push($errors, "Mot de passe nécessaire"); }
@@ -26,13 +26,12 @@ if (isset($_POST['reg_user'])) {
 	array_push($errors, "Les deux mots de passe ne sont pas identiques");
   }
 
-  // first check the database to make sure 
-  // a user does not already exist with the same username and/or email
+  // Verification que la personne n'est pas déjà inscrite
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
-  if ($user) { // if user exists
+  if ($user) { // SI l'utilisateur existe déjà ->
     if ($user['username'] === $username) {
       array_push($errors, "Identifiant déjà existant");
     }
@@ -44,7 +43,7 @@ if (isset($_POST['reg_user'])) {
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-  	$password = md5($password_1);//encrypt the password before saving in the database
+  	$password = md5($password_1);//Cryptage 
 
   	$query = "INSERT INTO users (username, email, password) 
   			  VALUES('$username', '$email', '$password')";
@@ -55,7 +54,7 @@ if (isset($_POST['reg_user'])) {
   }
 }
 
-// LOGIN USER
+// Page de connexion
 if (isset($_POST['login_user'])) {
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $password = mysqli_real_escape_string($db, $_POST['password']);
