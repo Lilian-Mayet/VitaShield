c61cc5b980a860800f5236bfe00d5d3db5e6940f
lilian <mayetlilian@gmail.com>
2023-06-05 20:18:56 +0200

    mailer?

--- server.php
+++ server.php
@@ -3,13 +3,16 @@
 session_start();
 $db = mysqli_connect('herogu.garageisep.com', 'Cu7dIKBPWC_vitashield', 'CqT0EXXRXWyBvVh3', 'HwKgNc7yDt_vitashield');
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 
 require 'PHPMailer-master\src\Exception.php';
 require 'PHPMailer-master\src\PHPMailer.php';
 require 'PHPMailer-master\src\SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+
 
 
 function calculerAge($dateNaissance) {
